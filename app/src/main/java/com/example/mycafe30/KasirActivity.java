e82d8955e19585652b594bae338f5215c2c7f968
Hilal Arsa <hilalarsa@gmail.com>
2019-12-22 18:56:32 +0700

    add condition on user login as admin / kasir

--- app/src/main/java/com/example/mycafe30/KasirActivity.java
+++ app/src/main/java/com/example/mycafe30/KasirActivity.java
@@ -1,19 +1,26 @@
 package com.example.mycafe30;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class KasirActivity extends AppCompatActivity {
+    Button btnMenu, btnMeja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kasir);
+
+        initView();
+        initListener();
     }
 
     //menu logout
@@ -38,5 +45,29 @@ public class KasirActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void initView(){
+        btnMenu = findViewById(R.id.btnMenu);
+        btnMeja = findViewById(R.id.btnMeja);
+    }
+
+    public void initListener(){
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this method is actually performing the write operation
+//                Intent intent = new Intent(KasirActivity.this, ListCafe.class);
+//                startActivity(intent);
+            }
+        });
+        btnMeja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this method is actually performing the write operation
+//                Intent intent = new Intent(KasirActivity.this, ListUser.class);
+//                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mycafe30/LoginActivity.java
+++ app/src/main/java/com/example/mycafe30/LoginActivity.java
@@ -2,27 +2,73 @@ package com.example.mycafe30;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mycafe30.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     Button btLogin;
+    EditText username, password;
+
+    DatabaseReference databaseReference;
+    List<User> Users;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin = findViewById(R.id.btLogin);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users");
+
+
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String usernameFromEditText = username.getText().toString();
+                final String passwordFromEditText = password.getText().toString();
+                databaseReference.orderByChild("username").equalTo(usernameFromEditText).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot user : dataSnapshot.getChildren()) {
+                            User userObj = user.getValue(User.class);
+
+                            if (userObj.getPassword().equals(passwordFromEditText)) {
+                                Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.d("TAG", databaseError.toString());
+                    }
+                });
+
                 //this method is actually performing the write operation
-                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+//                startActivity(intent);
             }
         });
     }
