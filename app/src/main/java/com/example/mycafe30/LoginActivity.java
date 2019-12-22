e517872b7a1a4649d8dd989e6da25b998cebd39e
DESKTOP-1EMEQ9P\Asus <auliaikam@gmail.com>
2019-12-22 19:04:50 +0700

    mau pull login

--- app/src/main/java/com/example/mycafe30/LoginActivity.java
+++ app/src/main/java/com/example/mycafe30/LoginActivity.java
@@ -4,26 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.mycafe30.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.Security;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    DatabaseReference databaseReferences;
     Button btLogin;
+    EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btLogin = (Button) findViewById(R.id.btLogin);
-
-        btLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //this method is actually performing the write operation
-                Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
-                startActivity(intent);
-            }
-        });
     }
+
 }
