3fc59100336440d607f6400f932b89cb515a7a7d
DESKTOP-1EMEQ9P\Asus <auliaikam@gmail.com>
2019-12-22 15:47:25 +0700

    user interface

---
+++ app/src/main/java/com/example/mycafe30/KasirActivity.java
@@ -0,0 +1,42 @@
+package com.example.mycafe30;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KasirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kasir);
+    }
+
+    //menu logout
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.menuLogout:
+                SharedPreferences setting = getSharedPreferences("key", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = setting.edit();
+                editor.remove("username");
+                editor.remove("password");
+                editor.commit();
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
